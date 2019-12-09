d133d5308ed91579ce21acde7112191d63240b49
Nishad Aherrao <nishad.aherrao10@gmail.com>
2019-12-08 23:56:29 -0600

    Added kick and ban for moderators, can also ban multiple at same time

--- bot.js
+++ bot.js
@@ -16,6 +16,7 @@ const {
   priceTemplateUpbit,
   priceTemplateFinexbox
 } = priceData
+
 client.on('message', msg => {
   if (msg.content === '!ping') {
     msg.channel.send('pong')
@@ -133,6 +134,127 @@ client.on('message', msg => {
         )
       )
       .catch(error => console.log(error))
+  }
+})
+
+client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return
+
+  // If the message content starts with "!kick"
+  if (message.content.startsWith('!kick')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
+
+    if (message.member.hasPermission('KICK_MEMBERS', false, false)) {
+      Array.from(message.mentions.users, ([key, value]) => {
+        const user = value
+        // const user = message.mentions.users.first()
+        // If we have a user mentioned
+        if (user) {
+          if (user.id !== '653386053356617768') {
+            // Now we get the member from the user
+            const member = message.guild.member(user)
+            // If the member is in the guild
+            if (member && user.id !== `653386053356617768`) {
+              /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+              member
+                .kick('Kicked by bot using command.')
+                .then(() => {
+                  // We let the message author know we were able to kick the person
+                  client.channels
+                    .get('652982383003435008')
+                    .send(`Successfully kicked ${user.tag}`)
+                })
+                .catch(err => {
+                  // An error happened
+                  // This is generally due to the bot not being able to kick the member,
+                  // either due to missing permissions or role hierarchy
+                  message.reply('I was unable to kick the member')
+                  // Log the error
+                  console.error(err)
+                })
+            } else {
+              // The mentioned user isn't in this guild
+              message.reply("That user isn't in this guild!")
+            }
+            // Otherwise, if no user was mentioned
+          } else {
+            message.reply("You didn't mention the user to kick!")
+          }
+        } else {
+          message.reply('Dont kick me!')
+        }
+      })
+    } else {
+      message.reply('You dont have the permission to kick people.')
+    }
+  }
+})
+
+client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return
+
+  // if the message content starts with "!ban"
+  if (message.content.startsWith('!ban')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
+
+    if (message.member.hasPermission('KICK_MEMBERS', false, false)) {
+      Array.from(message.mentions.users, ([key, value]) => {
+        const user = value
+        // If we have a user mentioned
+        if (user) {
+          if (user.id !== '653386053356617768') {
+            // Now we get the member from the user
+            const member = message.guild.member(user)
+            // If the member is in the guild
+            if (member) {
+              /**
+         * Ban the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         * Read more about what ban options there are over at
+         * https://discord.js.org/#/docs/main/stable/class/GuildMember?scrollTo=ban
+         */
+              member
+                .ban({
+                  reason: 'They were bad!'
+                })
+                .then(() => {
+                  // We let the message author know we were able to ban the person
+                  client.channels
+                    .get('652982383003435008')
+                    .send(`Successfully kicked ${user.tag}`)
+                })
+                .catch(err => {
+                  // An error happened
+                  // This is generally due to the bot not being able to ban the member,
+                  // either due to missing permissions or role hierarchy
+                  message.reply('I was unable to ban the member')
+                  // Log the error
+                  console.error(err)
+                })
+            } else {
+              // The mentioned user isn't in this guild
+              message.reply("That user isn't in this guild!")
+            }
+          } else {
+            // Otherwise, if no user was mentioned
+            message.reply("You didn't mention the user to ban!")
+          }
+        } else {
+          message.reply('Dont ban me!')
+        }
+      })
+    } else {
+      message.reply('You dont have the permission to ban people.')
+    }
   }
 })
 
