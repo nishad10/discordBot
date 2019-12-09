f340bcd37c3bbb3915589de0154e687b52d0ba73
Nishad Aherrao <nishad.aherrao10@gmail.com>
2019-12-08 18:21:51 -0600

    ping commad added with basic setup in place

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Works')
+require('dotenv').config()
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const dotenv = require('dotenv')
+
+const token = process.env.botToken
+/*
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+*/
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong')
+  }
+})
+client.login(token)
